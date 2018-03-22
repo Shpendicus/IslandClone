9005da96185659e7d3e0bf3a2087726e04135058
Shpendicus <Shpendicus@users.noreply.github.com>
2018-03-22 14:21:29 +0100

    moved the Debug-class in one single file

---
+++ Source/Debug.pas
@@ -0,0 +1,18 @@
+﻿namespace RemObjects.Elements.System;
+
+type
+  Debug = public static class
+  public
+    [Conditional('DEBUG')]
+    class method Assert(aCheck: Boolean; aMessage: String; aFile: String := currentFileName(); aLine: Integer := currentLineNumber());
+    begin
+      if not aCheck then
+        raise new AssertionException('Assertion failed: '+aMessage+' at '+aFile+'('+aLine+')');
+    end;
+    class method Throw(s: String);
+    begin
+      raise new Exception(s);
+    end;
+  end;
+
+end.
